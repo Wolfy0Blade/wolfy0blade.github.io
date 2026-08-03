2b3e1bbef37366875bbf991432e53813817ab677
Wolfy0Blade <astronautwolfy6489@gmail.com>
2026-08-02 21:20:16 -0300

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://wrjzjedcobxowewazeul.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_wmZVYsc-1EJEP4ZtrK4vDQ_faA7-x26";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
